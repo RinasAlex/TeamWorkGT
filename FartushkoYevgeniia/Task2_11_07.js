4ea6385223a3f7127c5acfa24d95aeaa81a4e688
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-11 10:14:37 +0200

    add task2

--- FartushkoYevgeniia/Task2_11_07.js
+++ FartushkoYevgeniia/Task2_11_07.js
@@ -1,4 +1,5 @@
 db.users.deleteMany(
-    { is_blocked: { $ne: true } },
-    { country: { $in: ['China', 'Brazil'] } }
+    { is_blocked: true },
+    { country: { $in: ['China', 'Brazil'] } },
+    { balance: { $lt: 0 } }
 )
