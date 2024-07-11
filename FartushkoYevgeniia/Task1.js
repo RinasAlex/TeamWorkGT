58f8f600793f4a7e7d2a5dfd56fc59c91914f623
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-11 09:47:19 +0200

    add task1

--- FartushkoYevgeniia/Task1.js
+++ FartushkoYevgeniia/Task1.js
@@ -1,3 +1,5 @@
-db.users.countDocuments(
-    {country: {$in: ['Germany', 'France']}}
-)
+db.products.insertMany([
+    { title: 'Product1', price: 5.5, quantity: 25 },
+    { title: 'Product2', price: 15, quantity: 10 },
+    { title: 'Product3', price: 2.55, quantity: 50 }
+])
