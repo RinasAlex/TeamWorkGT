0ac2dee5ec4cbbc41966714475280db506f99ce7
Trinity <alles.aug.2000@gmail.com>
2024-07-11 09:47:33 +0200

    update

---
+++ Tatiana/Task1.js
@@ -0,0 +1,6 @@
+
+db.products.insertMany([
+    { title: 'Product1', price: 2.5, quantity: 10},
+    { title: 'Product2', price: 9.8, quantity: 50},
+    { title: 'Product3', price: 1.5, quantity: 3}
+])
