b43020290bd052082388dd631784de03b3804af4
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-07-11 10:34:55 +0200

    update new task

--- Yelyzaveta_Shydenko/task4.js
+++ Yelyzaveta_Shydenko/task4.js
@@ -1,5 +1,12 @@
-db.products.find({
-  price: { $gte: 10, $lte: 100 },
-  quantity: { $gte: 5 },
-});
+db.products.find(
+  {
+    price: { $gte: 10, $lte: 100 },
+    quantity: { $gte: 5 },
+  },
+  {
+    title: 1,
+    price: 1,
+    _id: 0,
+  }
+);
 
