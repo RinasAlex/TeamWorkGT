f677093d81ecfb08ea13d57e0162c7e7ef47b183
Julia <pasichnyk.julia2@gmail.com>
2024-07-11 09:52:52 +0200

    task

---
+++ task4.js
@@ -0,0 +1,17 @@
+db.products.insertMany([
+  {
+    title: "dress",
+    price: 10,
+    quantity: 50,
+  },
+  {
+    title: "t-shirt",
+    price: 20,
+    quantity: 20,
+  },
+  {
+    title: "bag",
+    price: 30,
+    quantity: 30,
+  },
+]);
