a0f6e53300fc444529fc3552ef544214f6dc0597
VoytovychM <mary.voytovych@gmail.com>
2024-07-11 10:26:26 +0200

    corrected task

--- MVoytovych/task4.js
+++ MVoytovych/task4.js
@@ -1,7 +1,9 @@
 db.users.deleteMany(
- 
-    { is_blocked: true },
-    { balance: { $lt: 0 } },
-    { country: { $in: ["China", "Brazil"] } }
-  
+
+    {
+        is_blocked: true,
+        balance: { $lt: 0 },
+        country: { $in: ["China", "Brazil"] }
+    }
+
 )
