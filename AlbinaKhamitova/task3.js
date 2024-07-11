9216c0eb3e825709b8ce8beb2502aaea82ff8c65
Albina Khamitova <albina757@yahoo.com>
2024-07-11 09:46:50 +0200

    added task3

---
+++ AlbinaKhamitova/task3.js
@@ -0,0 +1,5 @@
+db.products.insertMany(
+    { title: 'cap', price: 12.0, quantity: 60 },
+    { title: 'book', price: 22.0, quantity: 100 },
+    { title: 'T-shirt', price: 29.0, quantity: 200 },
+)
