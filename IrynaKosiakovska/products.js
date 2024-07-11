e4d50f434c64268d61871229ef81a53618b25640
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-07-11 09:45:59 +0200

    updated 3 products

--- IrynaKosiakovska/products.js
+++ IrynaKosiakovska/products.js
@@ -1,5 +1,6 @@
-db.products.insertMany(
+db.products.insertMany([
+
     { title: 'trainers', price: 120.0, quantity: 6 },
     { title: 'jacket', price: 210.0, quantity: 10 },
-    { title: 'tracksuit', price: 180.0, quantity: 20 },
-)
+    { title: 'tracksuit', price: 180.0, quantity: 20 }
+])
