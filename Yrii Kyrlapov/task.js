fcb7861787dac5a5a60ef2f11b48c8a03503209c
YuriiKyrlapov <151627070+YuriiKyrlapov@users.noreply.github.com>
2024-07-11 10:02:26 +0200

    test

---
+++ Yrii Kyrlapov/task.js
@@ -0,0 +1,9 @@
+
+db.products.insertMany([
+
+  { name: "Product 1", price: 100, category: "Category A" },
+  
+  { name: "Product 2", price: 200, category: "Category B" },
+  
+  { name: "Product 3", price: 300, category: "Category C" }
+])
