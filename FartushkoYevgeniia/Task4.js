b21fae53743bfdc9642ea5edfacb6485f7f90ed7
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-07-11 11:33:30 +0200

    add task4

---
+++ FartushkoYevgeniia/Task4.js
@@ -0,0 +1,10 @@
+db.products.updateMany(
+    {
+        quantity: { $gte: 5 },
+        price: { $lt: 1000 }
+    },
+    {
+        $mul: { price: 0.85 },
+        $inc: { quantity: -1 }
+    }
+)
