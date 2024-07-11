d51d7a830d4324c18d229dc0b0250824088ebea5
Alisakono <alisa.konovalov@web.de>
2024-07-11 10:09:21 +0200

    Breaking,Feature,Fix

---
+++ tasks/products.js
@@ -0,0 +1,18 @@
+db.products.insertMany(
+    [
+        {
+            title: "Icon",
+            price: 10.0,
+            quantity: 100
+        },
+        {
+            title: "Flag",
+            price: 10.0,
+            quantity: 30
+        },
+        {
+            title: "Pen",
+            price: 15.0,
+            quantity: 30
+        }
+    ])
